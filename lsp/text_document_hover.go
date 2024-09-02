18c698c1a45ae61eca37860f4bd48dceb7899fc8
Lukas Weber <l.weber@traveltrex.com>
2024-09-02 13:59:50 +0200

    wip server

---
+++ lsp/text_document_hover.go
@@ -0,0 +1,16 @@
+package lsp
+
+import (
+	"github.com/tliron/glsp"
+	protocol "github.com/tliron/glsp/protocol_3_16"
+)
+
+func text_document_hover(context *glsp.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
+	result := protocol.Hover{
+		Contents: protocol.MarkupContent{
+			Kind:  protocol.MarkupKindMarkdown,
+			Value: "Höverst du mich gerade?",
+		},
+	}
+	return &result, nil
+}
